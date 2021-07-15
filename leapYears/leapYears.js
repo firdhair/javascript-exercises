ebecfb6d8592019ad9e9d44f07f79af711f000d8
fira <firdhair30@gmail.com>
2021-07-16 00:34:55 +0700

    mengerjakan leapYear.js

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+function leapYears(year) {
+  let year1 = year;
 
-};
+  if (year1 % 100 === 0) {
+    if (year1 % 400 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (year1 % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = leapYears;
