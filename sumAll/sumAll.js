1e28373da8c150c3bf616ab5911cd7561e4fd549
fira <firdhair30@gmail.com>
2021-07-15 21:10:53 +0700

    mengerjakan sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+function sumAll(valueA, valueB) {
+  let total = 0;
 
-};
+  if (
+    valueA < 0 ||
+    valueB < 0 ||
+    typeof valueB !== "number" ||
+    typeof valueA !== "number"
+  ) {
+    return "ERROR";
+  } else if (valueA < valueB) {
+    for (let i = valueA; i <= valueB; i++) {
+      total += i;
+    }
+    return total;
+  } else if (valueA > valueB) {
+    for (let i = valueB; i <= valueA; i++) {
+      total += i;
+    }
+    return total;
+  }
+}
 
 module.exports = sumAll;
