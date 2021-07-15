704dd696fb884320bbfd95997a3ed0e23aa03080
fira <firdhair30@gmail.com>
2021-07-16 00:02:50 +0700

    mengerjakan tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function (suhu) {
+  let temp = suhu;
+  let convCel = ((suhu - 32) * 5) / 9;
+  let roundCel = Math.round(convCel * 10) / 10;
+  return roundCel;
 };
 
-const ctof = function() {
-
+const ctof = function (suhu) {
+  let temp = suhu;
+  let convFar = (suhu * 9) / 5 + 32;
+  let roundFar = Math.round(convFar * 10) / 10;
+  return roundFar;
 };
 
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
