78238e99c62f334ad4c500c44c03c97ab6386586
fira <firdhair30@gmail.com>
2021-07-15 14:59:42 +0700

    mengerjakan removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,13 +1,21 @@
-let myArr2 = [];
-function removeFromArray(a, b) {
+function removeFromArray(first, ...theArgs) {
   let myArr = [];
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b) {
-      // myArr += a[i];
-      myArr.push(a[i]);
-      //console.log(a[i]);
-    } else {
-      continue;
+  let args = [...theArgs];
+  // turn arguments into real array
+
+  for (let i = 0; i < first.length; i++) {
+    // to see if first[i] is found in args
+    let match = false; // not found yet
+    for (let j = 0; j < args.length; j++) {
+      if (first[i] === args[j]) {
+        // if found in args then continue to next array index
+        match = true;
+        continue;
+      }
+    }
+    // first[i] will be pushed to myArray if match wasn't found
+    if (!match) {
+      myArr.push(first[i]);
     }
   }
   return myArr;
