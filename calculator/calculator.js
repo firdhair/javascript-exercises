672d7b114599bb537a67ca96dd6b0aa049094b0d
fira <firdhair30@gmail.com>
2021-08-04 23:22:45 +0700

    finished calculator problems

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (theArgs) {
+  let sumNum = 0;
+  //let args = [...theArgs];
+
+  for (let i = 0; i < theArgs.length; i++) {
+    sumNum += theArgs[i];
+  }
+  return sumNum;
 };
 
-const multiply = function() {
-
+const multiply = function (multiNum) {
+  let multi = 1;
+  for (let i = 0; i < multiNum.length; i++) {
+    multi *= multiNum[i];
+  }
+  return multi;
 };
 
-const power = function() {
-	
+const power = function (aFirst, bFirst) {
+  let sumNum = 1;
+
+  for (let i = 0; i < bFirst; i++) {
+    sumNum *= aFirst;
+  }
+  return sumNum;
 };
 
-const factorial = function() {
-	
+const factorial = function (factNum) {
+  const ori = factNum; // keep factNum original value
+  let sumNum = 1;
+
+  for (let i = 0; i < ori; i++) {
+    sumNum *= factNum; // multiply with current factNum value
+    factNum--; // decrease factNum value by each loop
+  }
+  return sumNum;
 };
 
 module.exports = {
@@ -28,5 +50,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
