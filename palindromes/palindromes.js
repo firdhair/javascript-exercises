d9e3011608d3f2d5afbe848153f73c6a84a244a9
fira <firdhair30@gmail.com>
2021-08-05 00:39:54 +0700

    palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+function palindromes(palindro) {
+  let stringLower = palindro.toLowerCase().replace(/[^A-Za-z]/g, "");
 
-};
+  let stringSplit = stringLower.split("").reverse().join(""); // join them back
+
+  if (stringSplit === stringLower) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = palindromes;
