95300d34ce8a0a2d5f0cfba4678152e82f8d8c40
fira <firdhair30@gmail.com>
2021-08-07 20:34:47 +0700

    findTheOldest and getTheTitles exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+function findTheOldest(people) {
+  const currentYear = new Date().getFullYear();
 
-};
+  const oldest = people.sort(function (tes12, tes22) {
+    if (typeof tes12.yearOfDeath === "undefined") {
+      tes12.yearOfDeath = currentYear;
+    } else if (typeof tes22.yearOfDeath === "undefined") {
+      tes22.yearOfDeath = currentYear;
+    }
+
+    const personA = tes12.yearOfDeath - tes12.yearOfBirth;
+    const personB = tes22.yearOfDeath - tes22.yearOfBirth;
+
+    if (personA > personB) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  return oldest[0];
+}
 
 module.exports = findTheOldest;
